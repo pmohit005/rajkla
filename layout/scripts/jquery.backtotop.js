1c271be1713b5415a1bc0ed63c4e6fef2f8f6e9d
Mohit Panchal <pmohit005@gmail.com>
2021-03-31 13:22:48 +0530

    git initiated and scrolling effects are added

---
+++ layout/scripts/jquery.backtotop.js
@@ -0,0 +1,32 @@
+/*
+Template Name: Shiphile
+Author: <a href="https://www.os-templates.com/">OS Templates</a>
+Author URI: https://www.os-templates.com/
+Copyright: OS-Templates.com
+Licence: Free to use under our free template licence terms
+Licence URI: https://www.os-templates.com/template-terms
+File: Back to Top JS
+*/
+
+jQuery("#backtotop").click(function () {
+    jQuery("body,html").animate({
+        scrollTop: 0
+    }, 600);
+});
+jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() > 150) {
+        jQuery("#backtotop").addClass("visible");
+    } else {
+        jQuery("#backtotop").removeClass("visible");
+    }
+});
+
+jQuery("#scrliwtd,#scrliws,#scrlisa").click(function () {
+
+    jQuery("body,html").animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 600);
+});
+
+
+
